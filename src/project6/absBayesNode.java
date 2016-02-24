45183069fb20289313121fe7ddab123dac97c1b9
Rick Wight <fmwight@wpi.edu>
2016-02-24 13:43:49 -0500

    Commented absBayesNode

--- src/project6/absBayesNode.java
+++ src/project6/absBayesNode.java
@@ -2,22 +2,40 @@ package project6;
 
 import java.util.ArrayList;
 
+/**
+ * abstract class representing a node in a bayes net. Contains a list of edges 
+ * leading into and out from the node.
+ */
 public abstract class absBayesNode {
 	private ArrayList<Edge> edgesTo;
 	private ArrayList<Edge> edgesFrom;
 	
+	/**
+	 * add the given edge to edgesTo
+	 * @param anEdge
+	 */
 	public void addEdgeTo(Edge anEdge) {
 		edgesTo.add(anEdge);
 	}
 	
+	/**
+	 * add the given edge to edgesFrom
+	 * @param anEdge
+	 */
 	public void addEdgeFrom(Edge anEdge) {
 		edgesFrom.add(anEdge);
 	}
 	
+	/**
+	 * @return edgesFrom
+	 */
 	public ArrayList<Edge> getEdgesFrom() {
 		return edgesFrom;
 	}
 	
+	/**
+	 * @return edgesTo
+	 */
 	public ArrayList<Edge> getEdgesTo() {
 		return edgesTo;
 	}
