e3a71f276e40488bbdc9815998cbe3bfd9fdb632
Rick Wight <fmwight@wpi.edu>
2016-02-24 12:51:32 -0500

    removed comment

--- src/project6/makeBayesNet.java
+++ src/project6/makeBayesNet.java
@@ -3,8 +3,8 @@ package project6;
 public class makeBayesNet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// are we in dev?
+		// TODO Auto-generated method stub 
+		
 	}
 
 }
