e7225227a674f3fbba2c6523bf7e098360f026c8
Rick Wight <fmwight@wpi.edu>
2016-03-01 21:53:12 -0500

    added BooleanRef class to pass a boolean by reference

---
+++ src/project6/BooleanRef.java
@@ -0,0 +1,13 @@
+package project6;
+
+public class BooleanRef {
+	public boolean value;
+	
+	public BooleanRef(boolean aVal) {
+		value = aVal;
+	}
+	
+	public void setVal(boolean aVal) {
+		value = aVal;
+	}
+}
